0e7eebd8aae23a2edd52c352ce0ce6bc2e4e406f
Aravind K <aravindk@thetravelagentnextdoor.com>
2024-05-06 10:06:57 +0530

    fix: env var

--- src/configs.ts
+++ src/configs.ts
@@ -1,6 +1,9 @@
 export const APP_NAME = "link vault";
 
-export const CLIENT_URL = process.env.NEXT_PUBLIC_BASE_URL;
+export const CLIENT_URL =
+  process.env.NODE_ENV === "production"
+    ? "https://" + process.env.VERCEL_URL
+    : process.env.NEXT_PUBLIC_BASE_URL;
 
 // FORGOT PASSWORD TOKEN
 export const FORGOT_PASSWORD_TOKEN_LENGTH = 124;
