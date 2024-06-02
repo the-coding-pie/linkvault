974b96bdc733d37d88e543169ba864cd516e0c99
Aravind K <aravindk@thetravelagentnextdoor.com>
2024-06-02 22:10:05 +0530

    fix: key

--- src/app/admin/(home)/page.tsx
+++ src/app/admin/(home)/page.tsx
@@ -6,7 +6,7 @@ const AdminHome = async () => {
   return (
     <div className="container mt-8">
       {tempLinks.links.map((link) => (
-        <div className="p-2">
+        <div key={link.id} className="p-2">
           <h2 className="text-lg font-medium">{link.title}</h2>
           <div className="flex items-center gap-2">
             <button className="text-green-600">Accept</button>
